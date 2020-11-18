f6c58e0065cf8036c38e6cb22ee68d37e396a30f
Opilane TTHK <opilane@tthk.ee>
2020-11-18 14:57:34 +0200

    first commit

---
+++ ClearFolder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace ClearFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            DeleteAllfiles();
+
+            DirectoryInfo rootDirectory = new DirectoryInfo(rootPath);
+            foreach (DirectoryInfo dir in rootDirectory.GetDirectories())
+            {
+                DeleteAllFolders(dir.FullName, true);
+            }
+
+
+
+        }
+
+        public static void DeleteAllfiles()
+        {
+            string rootpath = @"C:\Users\opilane\samples";
+            DirectoryInfo directory = new DirectoryInfo(rootpath);
+
+            foreach(FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+
+        }
+
+        public static void DeleteAllFolders(string directoryName, bool ifExists)
+        {
+          if(Directory.Exists(directoryName))
+          {
+                Directory.Delete(directoryName, true);
+          }else if (ifExists)
+          {
+                throw new SystemException("No such directory to delete");
+          }
+
+        }
+
+    }
+}
